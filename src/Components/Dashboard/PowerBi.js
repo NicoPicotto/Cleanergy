61a68d5ef186161d1c610da42ba6eb37d391414a
Nico Picotto <nicolaspicoto@Nicos-MacBook-Pro.local>
2024-04-29 11:08:16 -0300

    tablero cambios

--- src/Components/Dashboard/PowerBi.js
+++ src/Components/Dashboard/PowerBi.js
@@ -1,39 +1,44 @@
-import React from 'react';
-import { Stack } from '@chakra-ui/react';
-import { useAuth } from '../../Context/Context';
-
-const powerBiEmbedUrls = {
-	//GV Biogas
-	O2fXCgvBShbB2cSKR15wcOuL4IN2:
-		'https://app.powerbi.com/view?r=eyJrIjoiZDgyNDA2YmQtYTU3Ny00NDk3LWIyZWItYjRhN2I1OTA5Mzk4IiwidCI6IjhkY2Q1YWZhLWJjMGMtNDI4ZS1hZmIwLTc4MDg5ZmI1NDRkNiIsImMiOjR9&pageName=ReportSection837859f26274cd55faa2',
-	//Alimentos Magros
-	t5x83S6zJRNfXE5BITl7A9TJt733:
-		'https://app.powerbi.com/view?r=eyJrIjoiMDFjZGVjODktZjhkMS00OTIwLTg0NDctYmZlNTBjZjc3Yzc0IiwidCI6IjhkY2Q1YWZhLWJjMGMtNDI4ZS1hZmIwLTc4MDg5ZmI1NDRkNiIsImMiOjR9&pageName=ReportSection7790c9c9b2bda7c220d8',
-	//Resner
-	Q6N4WP7tqCgsfx0tK2Aj0ZyuNWO2:
-		'https://app.powerbi.com/view?r=eyJrIjoiOTY3NmM0ZDItMDQ1OC00ZWE4LWEwMmMtYWQ4NGE2NjY0OGQ3IiwidCI6IjhkY2Q1YWZhLWJjMGMtNDI4ZS1hZmIwLTc4MDg5ZmI1NDRkNiIsImMiOjR9&pageName=ReportSection',
-	//3C Biogas
-	'3hX52ke6gQS4HG5Ah8I3PqNN9E13':
-		'https://app.powerbi.com/view?r=eyJrIjoiYjBiZDQ3MGItODcwYi00NWJmLWE4ZTctNTkwNTAyZDY1ZjMzIiwidCI6ImUzNjVlZjc3LTUxMzItNDIzMi04YWI5LWM5NzA1N2I2ODA0MyIsImMiOjR9&pageName=ReportSection88df21b5aa08eb3a1b36',
-};
+import React, { useEffect, useState } from "react";
+import { Stack } from "@chakra-ui/react";
+import { useAuth } from "../../Context/Context";
+import { query, where, getDocs, collection } from "firebase/firestore";
+import { firestore } from "../../firebase";
 
 const PowerBi = () => {
-	const { userUid } = useAuth();
+  const { userUid } = useAuth();
 
-	const embedUrl = powerBiEmbedUrls[userUid];
+  const [datos, setDatos] = useState(null);
 
-	return (
-		<Stack marginBlock='3em'>
-			<iframe
-				title='Report Section'
-				width='100%'
-				height='636'
-				src={embedUrl}
-				frameborder='0'
-				allowFullScreen={true}
-			></iframe>
-		</Stack>
-	);
+  useEffect(() => {
+    const getDatos = async () => {
+      const docs = [];
+      const q = query(
+        collection(firestore, "clientes"),
+        where("idcliente", "==", userUid)
+      );
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setDatos(docs);
+    };
+    getDatos();
+  }, []);
+
+  return (
+    <Stack marginBlock='3em'>
+      {datos && (
+        <iframe
+          title='Report Section'
+          width='100%'
+          height='636'
+          src={datos[0].powerBiApp}
+          frameborder='0'
+          allowFullScreen={true}
+        ></iframe>
+      )}
+    </Stack>
+  );
 };
 
 export default PowerBi;
